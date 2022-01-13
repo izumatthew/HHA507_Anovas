882e87e2addb9f857c34d06cdbadc3e9be1ffa4e
izumatthew <93498212+izumatthew@users.noreply.github.com>
2022-01-13 18:07:57 -0500

    Add files via upload

---
+++ Anova.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 13 12:40:31 2022
+
+@author: izuchukwumatthew
+"""
+
+def rules(row):
+  if row['COVID-19_Deaths'] <= 5000 :
+    return 'abnormal'
+  elif row['COVID-19_Deaths'] <= 25000:
+    return 'normal'
+  elif row ['COVID-19_Deaths'] > 45000:
+    return 'high'
+
+
+def['COVID-19 Deaths_mod2'] = df.apply(rules,1)
+
+df[['COVID-19 Deaths', 'COVID-19 Deaths_mod2']]
+
+Is there significance between the number of COVID Deaths and age group?
+
+model = ols('COVID-19 Deaths ~ C(Age Group)', data=df).fit()
+anova_table = sm.stats.anova_lm(model, typ=1)
+anova_table
+
+Significant difference or result
+
+Is there significance between the number of COVID Deaths and age group?
+
+
+model = ols('COVID-19 Deaths ~ C(State)', data=df).fit()
+anova_table = sm.stats.anova_lm(model, typ=1)
+anova_table
+
+No significant difference
+
+Is there significance between the number of COVID Deaths and age group?
+
+
+model = ols('COVID-19 Deaths ~ C(Condition)', data=df).fit()
+anova_table = sm.stats.anova_lm(model, typ=1)
+anova_table
+
+No significant difference
